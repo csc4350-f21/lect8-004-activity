e37945c63fd3769c19de8e9d970d4c4769abbb14
Ubuntu <vjefhsb@semkeem.yqqvdj3k4zku3povoohgviujwa.bx.internal.cloudapp.net>
2021-09-21 17:15:21 +0000

    first commit

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ def index():
     return render_template("index.html")
 
 app.run(
-    port=int(os.getenv("PORT", "8080")),
-    host=os.getenv("IP", "0.0.0.0")
+    debug =True
+    #port=int(os.getenv("PORT", "8080")),
+    #host=os.getenv("IP", "0.0.0.0")
 )
 
